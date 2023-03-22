f81c1753d781e389f7e311378d07dcc986b71c81
Angelika <angelika@sonic.net.pl>
2023-03-22 17:26:37 +0100

    Fixes types and environment bug

--- src/app/interfaces/request.ts
+++ src/app/interfaces/request.ts
@@ -1,31 +1,4 @@
 export interface IRequest {
-  info: any;
+  info: unknown;
   results: any;
-}
-
-export interface IPaginatedRequest {
-  status: boolean;
-  data: IPaginatedData;
-}
-
-export interface IPaginatedData {
-  current_page: number;
-  data: any;
-  first_page_url: string;
-  from: number;
-  last_page: number;
-  last_page_url: string;
-  links: ILink[];
-  next_page_url?: any;
-  path: string;
-  per_page: string;
-  prev_page_url?: any;
-  to: number;
-  total: number;
-}
-
-export interface ILink {
-  url: string;
-  label: string;
-  active: boolean;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ import { EnvironmentModel } from '../app/core/models/enviroment/environment.mode
 
 export const environment: EnvironmentModel = {
   production: true,
+  api_url: 'https://randomuser.me/api/',
 };
